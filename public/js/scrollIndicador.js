cd21c843eb01bf736ce57ed9e59d0700689f8836
Luis Carlos <lgasmor2203@g.educaand.es>
2023-03-06 20:18:24 +0100

    progress-bar añadida

---
+++ public/js/scrollIndicador.js
@@ -0,0 +1,10 @@
+$(function(){
+    window.onscroll = function() {scroll()};
+})
+
+function scroll() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("barra").style.width = scrolled + "%";
+  }
